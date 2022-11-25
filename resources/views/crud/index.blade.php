97026fb575ae409ec42649a72ffb850785f0a0ee
Rafly Raditya AP <rafly.raditya21@student.smkti.net>
2022-11-25 09:25:43 +0800

    update kamis

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -2,48 +2,34 @@
 @section('content')
 <div class="bg-red-100">
     <div class="max-w-7xl mx--auto bg-white flex flex-col px-10 min-h-screen py-10 gap-10">
-        <div class="">
-            <h1 class="teks-lg">Judul</h1>
-            <span class="text-sm">
-                Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Ad quod vel id temporibus illum sapiente, 
-                delectus nam laboriosam voluptatibus doloribus, laudantium quidem fuga adipisci, 
-                quos voluptatem nulla eaque possimus aut.
-            </span>
-        </div>
-
-        <div class="">
-            <h1 class="teks-lg">Judul</h1>
-            <span class="text-sm">
-                Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Ad quod vel id temporibus illum sapiente, 
-                delectus nam laboriosam voluptatibus doloribus, laudantium quidem fuga adipisci, 
-                quos voluptatem nulla eaque possimus aut.
-            </span>
-        </div>
-
-        <div class="">
-            <h1 class="teks-lg">Judul</h1>
-            <span class="text-sm">
-                Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Ad quod vel id temporibus illum sapiente, 
-                delectus nam laboriosam voluptatibus doloribus, laudantium quidem fuga adipisci, 
-                quos voluptatem nulla eaque possimus aut.
-            </span>
-        </div>
-
-        <div class="">
-            <h1 class="teks-lg">Judul</h1>
-            <span class="text-sm">
-                Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Ad quod vel id temporibus illum sapiente, 
-                delectus nam laboriosam voluptatibus doloribus, laudantium quidem fuga adipisci, 
-                quos voluptatem nulla eaque possimus aut.
-            </span>
-        </div>
-    </div>
-
 </div>
+<table>
+    <tr>
+        <th class="px-4 py-2 border">No</th>
+        <th class="px-4 py-2 border">Judul</th>
+        <th class="px-4 py-2 border">Konten</th>
+        <th class="px-4 py-2 border">Aksi</th>
+    </tr>
+    @@forelse ($konten as $index => $item)
+    <tr>
 
+        <th class="px-4 py-2 border">{{ $index + 1 }}</th>
+        <th class="px-4 py-2 border">{{ $item->Judul}}</th>
+        <th class="px-4 py-2 border">{{ $item->konten}}</th>
+        <th class="px-4 py-2 border">
+            <div>
+                <a href="">Edit</a>
+                <a href="">Delete</a>
+            </div>
+        </th>
+            
+        @empty
+    </tr>
+
+    <tr>
+        <th colspan="12" class="px-4 py-2 border">Tidak Ada Konten</th>
+    </tr>
+    @endforelse
+</table>
     
 @endsection
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,15 +2,13 @@
     <div class="flex flex-row gap-10">
         <a class="bg-gray-200 hover:bg-gray-400 px-6 py-2 rounded-md"
         href="">
-            
-        Halaman 1</a>
-        <a class="bg-gray-200 hover:bg-gray-400 px-6 py-2 rounded-md"
-        href="">
-        Halaman 2</a>
+        Content</a>
+
 
         <a class="bg-gray-200 hover:bg-gray-400 px-6 py-2 rounded-md"
-        href="">    
-        Halaman 3</a>
+        href="{{ route('crud.index') }}">
+        CRUD</a>
+
 
     </div>
 </div>
